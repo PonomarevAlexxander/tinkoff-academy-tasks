483b0d4bf3e98054bae6e5bb23d1e82d65c80357
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-10-06 18:32:55 +0300

    Complete 1 Task

---
+++ src/main/java/edu/hw1/VideoUtils.java
@@ -0,0 +1,19 @@
+package edu.hw1;
+
+import java.util.Objects;
+
+public class VideoUtils {
+
+    private VideoUtils() {
+    }
+
+    public static int minutesToSeconds(String time) {
+        Objects.requireNonNull(time);
+        String[] minutesAndSeconds = time.split(":");
+        if (minutesAndSeconds.length != 2) {
+            return -1;
+        }
+        int seconds = Integer.parseInt(minutesAndSeconds[1]);
+        return seconds >= 60 ? -1 : Integer.parseInt(minutesAndSeconds[0]) * 60 + seconds;
+    }
+}
