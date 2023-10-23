e705d0c027c85f742d75a682cf3d811ce56da4c3
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-10-24 00:27:20 +0300

    Devide test into two different one

--- src/test/java/edu/hw2/task3/FaultyConnectionTest.java
+++ src/test/java/edu/hw2/task3/FaultyConnectionTest.java
@@ -1,12 +1,8 @@
 package edu.hw2.task3;
 
 import java.util.Random;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
@@ -16,31 +12,33 @@ class FaultyConnectionTest {
     private static final double SUCCESS_PROBABILITY = 0.9;
 
     @Test
-    @DisplayName("testFaultyConnection_execute")
-    void testFaultyConnection_execute() {
+    @DisplayName("test FaultyConnection.execute to fail")
+    void test_FaultyConnection_execute_to_fail() {
         try (FaultyConnection connection = new FaultyConnection(new Random() {
             @Override
             public double nextDouble() {
                 return FAULTY_PROBABILITY;
             }
-        });) {
+        })) {
             Throwable exception = catchThrowable(() -> connection.execute(""));
             assertThat(exception)
                 .isInstanceOf(ConnectionException.class);
-        } catch (Exception ignored) {
         }
+    }
+
+    @Test
+    @DisplayName("test FaultyConnection.execute to success")
+    void test_FaultyConnection_execute_to_success() {
         try (FaultyConnection connection = new FaultyConnection(new Random() {
             @Override
             public double nextDouble() {
                 return SUCCESS_PROBABILITY;
             }
-        });) {
+        })) {
             Throwable exception = catchThrowable(() -> connection.execute(""));
             assertThat(exception)
                 .isNull();
-        } catch (Exception ignored) {
         }
-
     }
 
 }
