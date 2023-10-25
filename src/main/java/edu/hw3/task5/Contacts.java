baa770054d39547bf608928cf72c829d4cb64c76
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-10-25 17:37:16 +0300

    Complete 5th task

---
+++ src/main/java/edu/hw3/task5/Contacts.java
@@ -0,0 +1,38 @@
+package edu.hw3.task5;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Contacts {
+    private static final String ASCENDING_ORDER = "ASC";
+    private static final String DESCENDING_ORDER = "DESC";
+
+    public static List<Object> parseContacts(List<String> contacts, String compareFormat) {
+        if (compareFormat == null ||
+            !(compareFormat.equals(ASCENDING_ORDER) || compareFormat.equals(DESCENDING_ORDER))) {
+            throw new IllegalArgumentException("provided compare format is not available");
+        }
+        if (contacts == null) {
+            return new ArrayList<>();
+        }
+
+        List<String> sortedContacts = new ArrayList<>(contacts);
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] firstName = o1.split(" ");
+                String[] secondName = o2.split(" ");
+                if (firstName.length + firstName.length != 4) {
+                    return firstName[0].compareTo(secondName[0]);
+                }
+                return firstName[1].compareTo(secondName[1]);
+            }
+        };
+        if (compareFormat.equals(DESCENDING_ORDER)) {
+            comparator = comparator.reversed();
+        }
+        sortedContacts.sort(comparator);
+        return new ArrayList<>(sortedContacts);
+    }
+}
---
+++ src/test/java/edu/hw3/task5/ContactsTest.java
@@ -0,0 +1,56 @@
+package edu.hw3.task5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class ContactsTest {
+
+    @ParameterizedTest
+    @MethodSource("provideValidContacts")
+    void test_Contacts_parsContacts_on_valid_data(List<String> contacts, String compareOrder, List<Object> exprexted) {
+        assertThat(Contacts.parseContacts(contacts, compareOrder))
+            .isEqualTo(exprexted);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"SOME", "RANDOM", "STRING"})
+    @NullSource
+    void test_Contacts_parsContacts_with_invalid_compare_type(String compareOrder) {
+        assertThatThrownBy(() -> Contacts.parseContacts(List.of("Hi Mikki", "Li Chon", "Mike Corleone"), compareOrder))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideValidContacts() {
+        return Stream.of(
+            Arguments.of(
+                List.of("John Locke", "Thomas Aquinas", "David Hume", "Rene Descartes"),
+                "ASC",
+                List.of("Thomas Aquinas", "Rene Descartes", "David Hume", "John Locke")
+            ),
+            Arguments.of(
+                List.of("Paul Erdos", "Leonhard Euler", "Carl Gauss"),
+                "DESC",
+                List.of("Carl Gauss", "Leonhard Euler", "Paul Erdos")
+            ),
+            Arguments.of(
+                new ArrayList<String>(),
+                "DESC",
+                new ArrayList<>()
+            ),
+            Arguments.of(
+                null,
+                "DESC",
+                new ArrayList<>()
+            )
+        );
+    }
+
+}
