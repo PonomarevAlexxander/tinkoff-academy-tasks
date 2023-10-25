8f4f9e6eab4658949bb1a1d30d02122fd0494925
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-10-25 11:50:50 +0300

    Get List with List.of

--- src/main/java/edu/project1/HangmanGameRunner.java
+++ src/main/java/edu/project1/HangmanGameRunner.java
@@ -1,6 +1,6 @@
 package edu.project1;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class HangmanGameRunner {
@@ -11,9 +11,7 @@ public class HangmanGameRunner {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             ConsoleWriter console = new ConsoleWriter(System.out);
-            GameDictionary dictionary = new GameDictionary(new ArrayList<>() { {
-                add("godfather");
-            } });
+            GameDictionary dictionary = new GameDictionary(List.of("godfather"));
             GallowsProvider gallows = new ClassicGallowsProvider();
             HangmanGame gameDriver = new HangmanGame(dictionary, gallows, console, scanner);
 
