14c28eddc8ce60b03cb7f3348e19d9ab3b6ff04b
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-14 22:44:00 +0300

    Add new args in Cell usage

--- src/test/java/edu/project2/BreadthFirstSolverTest.java
+++ src/test/java/edu/project2/BreadthFirstSolverTest.java
@@ -9,16 +9,16 @@ class BreadthFirstSolverTest {
     @Test
     void test_solve_on_valid_data() {
         Cell[][] cells = new Cell[][] {
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL),
-                new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.PASSAGE),
-                new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL),
-                new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.PASSAGE),
-                new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL),
-                new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL)}
+            {new Cell(Cell.CellType.WALL, 0, 0), new Cell(Cell.CellType.WALL, 0, 1), new Cell(Cell.CellType.WALL, 0, 2),
+                new Cell(Cell.CellType.WALL, 0, 3), new Cell(Cell.CellType.WALL, 0, 4)},
+            {new Cell(Cell.CellType.WALL, 1, 0), new Cell(Cell.CellType.WALL, 1, 1), new Cell(Cell.CellType.PASSAGE, 1, 2),
+                new Cell(Cell.CellType.PASSAGE, 1, 3), new Cell(Cell.CellType.WALL, 1, 4)},
+            {new Cell(Cell.CellType.WALL, 2, 0), new Cell(Cell.CellType.PASSAGE, 2, 1), new Cell(Cell.CellType.WALL, 2, 2),
+                new Cell(Cell.CellType.PASSAGE, 2, 3), new Cell(Cell.CellType.WALL, 2, 4)},
+            {new Cell(Cell.CellType.WALL, 3, 0), new Cell(Cell.CellType.PASSAGE, 3, 1), new Cell(Cell.CellType.PASSAGE, 3, 2),
+                new Cell(Cell.CellType.PASSAGE, 3, 3), new Cell(Cell.CellType.WALL, 3, 4)},
+            {new Cell(Cell.CellType.WALL, 4, 0), new Cell(Cell.CellType.WALL, 4, 1), new Cell(Cell.CellType.WALL, 4, 2),
+                new Cell(Cell.CellType.WALL, 4, 3), new Cell(Cell.CellType.WALL, 4, 4)}
         };
         Maze maze = new Maze(2, 2, cells);
         BreadthFirstSolver solver = new BreadthFirstSolver();
@@ -33,16 +33,16 @@ class BreadthFirstSolverTest {
     @Test
     void test_solve_on_invalid_data() {
         Cell[][] cells = new Cell[][] {
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL),
-                new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.PASSAGE),
-                new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL),
-                new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.PASSAGE),
-                new Cell(Cell.CellType.PASSAGE), new Cell(Cell.CellType.WALL)},
-            {new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL),
-                new Cell(Cell.CellType.WALL), new Cell(Cell.CellType.WALL)}
+            {new Cell(Cell.CellType.WALL, 0, 0), new Cell(Cell.CellType.WALL, 0, 1), new Cell(Cell.CellType.WALL, 0, 2),
+                new Cell(Cell.CellType.WALL, 0, 3), new Cell(Cell.CellType.WALL, 0, 4)},
+            {new Cell(Cell.CellType.WALL, 1, 0), new Cell(Cell.CellType.WALL, 1, 1), new Cell(Cell.CellType.PASSAGE, 1, 2),
+                new Cell(Cell.CellType.PASSAGE, 1, 3), new Cell(Cell.CellType.WALL, 1, 4)},
+            {new Cell(Cell.CellType.WALL, 2, 0), new Cell(Cell.CellType.PASSAGE, 2, 1), new Cell(Cell.CellType.WALL, 2, 2),
+                new Cell(Cell.CellType.PASSAGE, 2, 3), new Cell(Cell.CellType.WALL, 2, 4)},
+            {new Cell(Cell.CellType.WALL, 3, 0), new Cell(Cell.CellType.PASSAGE, 3, 1), new Cell(Cell.CellType.PASSAGE, 3, 2),
+                new Cell(Cell.CellType.PASSAGE, 3, 3), new Cell(Cell.CellType.WALL, 3, 4)},
+            {new Cell(Cell.CellType.WALL, 4, 0), new Cell(Cell.CellType.WALL, 4, 1), new Cell(Cell.CellType.WALL, 4, 2),
+                new Cell(Cell.CellType.WALL, 4, 3), new Cell(Cell.CellType.WALL, 4, 4)}
         };
         Maze maze = new Maze(2, 2, cells);
         BreadthFirstSolver solver = new BreadthFirstSolver();
