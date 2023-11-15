25ee287f364b4c021c230629eb892245dc6f8aa6
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-15 20:45:46 +0300

    Add 13 as Day constant

--- src/main/java/edu/hw5/task2/FridaysFinder.java
+++ src/main/java/edu/hw5/task2/FridaysFinder.java
@@ -8,13 +8,14 @@ import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class FridaysFinder {
+    private static final int DAY_OF_MONTH_13 = 13;
     private static final TemporalAdjuster NEXT_FRIDAY_13_TH = temporal -> {
         LocalDate date = LocalDate.from(temporal);
         int day = date.getDayOfMonth();
-        if (day > 13) {
-            date = date.withDayOfMonth(13).plusMonths(1);
+        if (day > DAY_OF_MONTH_13) {
+            date = date.withDayOfMonth(DAY_OF_MONTH_13).plusMonths(1);
         } else {
-            date = date.withDayOfMonth(13);
+            date = date.withDayOfMonth(DAY_OF_MONTH_13);
         }
         while (!date.getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
             date = date.plusMonths(1);
@@ -25,9 +26,8 @@ public class FridaysFinder {
     private FridaysFinder() {
     }
 
-    @SuppressWarnings("MagicNumber")
     public static List<LocalDate> findAllFriday13th(int year) {
-        LocalDate date = LocalDate.of(year, 1, 13);
+        LocalDate date = LocalDate.of(year, 1, DAY_OF_MONTH_13);
         List<LocalDate> fridays13th = new LinkedList<>();
         while (date.getYear() == year) {
             if (date.getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
--- src/main/java/edu/hw5/task5/PlateNumberValidator.java
+++ src/main/java/edu/hw5/task5/PlateNumberValidator.java
@@ -4,7 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PlateNumberValidator {
-    private static final Pattern PLATE_NUMBER_PATTERN = Pattern.compile("^[АВЕКМНОРСТХ]\\d{1,2}[1-9][АВЕКМНОРСТХ]{2}\\d{2,3}$");
+    private static final Pattern PLATE_NUMBER_PATTERN =
+        Pattern.compile("^[АВЕКМНОРСТХ]\\d{1,2}[1-9][АВЕКМНОРСТХ]{2}\\d{2,3}$");
 
     private PlateNumberValidator() {
     }
