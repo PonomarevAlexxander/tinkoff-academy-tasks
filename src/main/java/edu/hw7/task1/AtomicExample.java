428d3e824724803e5584d7889045440e84627ed5
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-27 20:12:13 +0300

    Use executor service

--- src/main/java/edu/hw7/task1/AtomicExample.java
+++ src/main/java/edu/hw7/task1/AtomicExample.java
@@ -1,21 +1,21 @@
 package edu.hw7.task1;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Stream;
 
 public class AtomicExample {
-    private static final AtomicLong COUNTER = new AtomicLong();
+    private static final AtomicLong COUNTER = new AtomicLong(0);
 
     private AtomicExample() {
     }
 
     public static long incrementBy(int num) {
-        try (ExecutorService executor = Executors.newFixedThreadPool(num)) {
-            CompletableFuture.runAsync(COUNTER::incrementAndGet, executor);
+        try (ExecutorService executor = Executors.newFixedThreadPool(4)) {
+            for (int times = 0; times < num; times++) {
+                CompletableFuture.runAsync(COUNTER::incrementAndGet, executor).join();
+            }
         }
         return COUNTER.get();
     }
