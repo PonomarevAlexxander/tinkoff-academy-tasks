a935bea0c50409dfe06521432898ba69c7c5dc7c
Alexander Ponomarev <aaponomarev04@gmail.com>
2023-12-19 12:23:05 +0300

    Refactor inharitance to deflect code duplication

--- src/main/java/edu/project4/generator/ParallelRenderer.java
+++ src/main/java/edu/project4/generator/ParallelRenderer.java
@@ -6,16 +6,12 @@ import edu.project4.domain.Point;
 import edu.project4.transformations.Transformation;
 import java.awt.Color;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.stream.Stream;
 
-public class ParallelRenderer implements Renderer {
+public class ParallelRenderer extends Renderer {
     private final double minX = -1.5;
     private final double maxX = 1.5;
     private final double minY = -1.5;
@@ -39,8 +35,8 @@ public class ParallelRenderer implements Renderer {
         FractalImage result = FractalImage.create(canvas.width(), canvas.height());
         try (ExecutorService executorService = Executors.newFixedThreadPool(nThreads)) {
             BoundingRectangle rectangle = new BoundingRectangle(minX, maxY, maxX - minX, maxY - minY);
-
-            Stream.generate(() -> CompletableFuture.runAsync(() -> this.renderSamples(
+            for (int thread = 0; thread < nThreads; thread++) {
+                executorService.execute(() -> this.renderSamples(
                     result,
                     samples / nThreads,
                     iterPerSample,
@@ -48,67 +44,20 @@ public class ParallelRenderer implements Renderer {
                     variations,
                     rectangle,
                     symetri
-                ), executorService))
-                .limit(nThreads)
-                .forEach(feature -> {
-                    try {
-                        feature.get();
-                    } catch (InterruptedException | ExecutionException e) {
-                        throw new RuntimeException("failed to complete task", e);
-                    }
-                });
+                ));
+            }
         }
         return result;
     }
 
-    @SuppressWarnings("MagicNumber")
-    private void renderSamples(
-        FractalImage result,
-        int samples,
-        int iterPerSample,
-        List<Transformation> afines,
-        List<Transformation> variations,
-        BoundingRectangle rectangle,
-        boolean symetri
-    ) {
-        int wormupIterations = 20;
-        for (int sample = 0; sample < samples; sample++) {
-            Point point = new Point(
-                ThreadLocalRandom.current().nextDouble(minX, maxX),
-                ThreadLocalRandom.current().nextDouble(minY, maxY)
-            );
-            for (int step = -wormupIterations; step < iterPerSample; step++) {
-                Transformation affin = afines.get(ThreadLocalRandom.current().nextInt(afines.size()));
-                point = affin.apply(point);
-                Transformation variation = variations.get(ThreadLocalRandom.current().nextInt(variations.size()));
-                point = variation.apply(point);
-                if (symetri && step % 2 == 0) {
-                    point = new Point(-point.x(), point.y());
-                }
-                if (step >= 0 && rectangle.contains(point)) {
-                    plot(result, rectangle, point, affin.getColor());
-                }
-            }
-        }
-    }
-
-    private void plot(FractalImage canvas, BoundingRectangle rectangle, Point point, Color affinColor) {
+    protected void plot(FractalImage canvas, BoundingRectangle rectangle, Point point, Color affinColor) {
         int x = canvas.width() - (int) Math.floor(((maxX - point.x()) / rectangle.width()) * canvas.width());
         int y = canvas.height() - (int) Math.floor(((maxY - point.y()) / rectangle.height()) * canvas.height());
         if (canvas.contains(x, y)) {
             lock.readLock().lock();
             Pixel pixel = canvas.pixel(x, y);
             lock.readLock().unlock();
-            if (pixel.hitCount() == 0) {
-                pixel = new Pixel(affinColor, 1, 0);
-            } else {
-                Color pixelColor = pixel.color();
-                int red = (pixelColor.getRed() + affinColor.getRed()) / 2;
-                int green = (pixelColor.getGreen() + affinColor.getGreen()) / 2;
-                int blue = (pixelColor.getBlue() + affinColor.getBlue()) / 2;
-                Color newColor = new Color(red, green, blue);
-                pixel = new Pixel(newColor, pixel.hitCount() + 1, 0);
-            }
+            pixel = updatePixel(pixel, affinColor);
             lock.writeLock().lock();
             canvas.updatePixel(x, y, pixel);
             lock.writeLock().unlock();
--- src/main/java/edu/project4/generator/Renderer.java
+++ src/main/java/edu/project4/generator/Renderer.java
@@ -1,11 +1,15 @@
 package edu.project4.generator;
 
+import edu.project4.domain.BoundingRectangle;
+import edu.project4.domain.Pixel;
+import edu.project4.domain.Point;
 import edu.project4.transformations.Transformation;
+import java.awt.Color;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-@FunctionalInterface
-public interface Renderer {
-    FractalImage render(
+public abstract class Renderer {
+    public abstract FractalImage render(
         FractalImage canvas,
         List<Transformation> afines,
         List<Transformation> variations,
@@ -13,4 +17,48 @@ public interface Renderer {
         short iterPerSample,
         boolean symetri
     );
+
+    protected void renderSamples(
+        FractalImage result,
+        int samples,
+        int iterPerSample,
+        List<Transformation> afines,
+        List<Transformation> variations,
+        BoundingRectangle rectangle,
+        boolean symetri
+    ) {
+        int wormupIterations = 20;
+        for (int sample = 0; sample < samples; sample++) {
+            Point point = new Point(
+                ThreadLocalRandom.current().nextDouble(rectangle.x(), rectangle.x() + rectangle.width()),
+                ThreadLocalRandom.current().nextDouble(rectangle.y() - rectangle.height(), rectangle.y())
+            );
+            for (int step = -wormupIterations; step < iterPerSample; step++) {
+                Transformation affin = afines.get(ThreadLocalRandom.current().nextInt(afines.size()));
+                point = affin.apply(point);
+                Transformation variation = variations.get(ThreadLocalRandom.current().nextInt(variations.size()));
+                point = variation.apply(point);
+                if (symetri && step % 2 == 0) {
+                    point = new Point(-point.x(), point.y());
+                }
+                if (step >= 0 && rectangle.contains(point)) {
+                    plot(result, rectangle, point, affin.getColor());
+                }
+            }
+        }
+    }
+
+    protected abstract void plot(FractalImage canvas, BoundingRectangle rectangle, Point point, Color affinColor);
+
+    protected Pixel updatePixel(Pixel pixel, Color affinColor) {
+        if (pixel.hitCount() == 0) {
+            return new Pixel(affinColor, 1, 0);
+        }
+        Color pixelColor = pixel.color();
+        int red = (pixelColor.getRed() + affinColor.getRed()) / 2;
+        int green = (pixelColor.getGreen() + affinColor.getGreen()) / 2;
+        int blue = (pixelColor.getBlue() + affinColor.getBlue()) / 2;
+        Color newColor = new Color(red, green, blue);
+        return new Pixel(newColor, pixel.hitCount() + 1, 0);
+    }
 }
