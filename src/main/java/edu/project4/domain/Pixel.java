fcf7f2817282940c15a53b38480b9ba0b295366b
Alexander Ponomarev <aaponomarev04@gmail.com>
2023-12-20 12:06:09 +0300

    Make mutable and threadsafe

--- src/main/java/edu/project4/domain/Pixel.java
+++ src/main/java/edu/project4/domain/Pixel.java
@@ -2,5 +2,43 @@ package edu.project4.domain;
 
 import java.awt.Color;
 
-public record Pixel(Color color, int hitCount, double normal) {
+public class Pixel {
+
+    private Color color;
+    private int hitCount;
+    private double normal;
+
+    public Pixel(Color color, int hitCount, double normal) {
+        this.color = color;
+        this.hitCount = hitCount;
+        this.normal = normal;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public synchronized void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getHitCount() {
+        return hitCount;
+    }
+
+    public synchronized void setHitCount(int hitCount) {
+        this.hitCount = hitCount;
+    }
+
+    public synchronized void increaseHitCount(int number) {
+        this.hitCount += number;
+    }
+
+    public double getNormal() {
+        return normal;
+    }
+
+    public synchronized void setNormal(double normal) {
+        this.normal = normal;
+    }
 }
